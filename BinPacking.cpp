c8f82349700edb0074d2b833097ce52461c2992e
Qdake <qiuqi4baichi@gmail.com>
2019-11-27 12:44:30 +0100

    Add files via upload

---
+++ BinPacking.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+class Bin {
+    public:
+    
+}
+class Bins {
+    public:
+    Bins(int m_):m(m_){
+        for (i{1};i<=m;i++)
+            content.push_back(Bin());
+    }
+    std::vector<Bin> content;
+    unsigned int m;
+
+}
+
+class 
